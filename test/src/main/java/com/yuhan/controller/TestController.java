be44cbe1c7160cdf01c8110f8295eba8bd8d60df
testr02 <hakid77@gmail.com>
2023-09-06 15:48:20 +0900

    test

--- test/src/main/java/com/yuhan/controller/TestController.java
+++ test/src/main/java/com/yuhan/controller/TestController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.yuhan.model.Product;
 import com.yuhan.model.Qa;
 import com.yuhan.model.User;
+import com.yuhan.model.Jproduct;
+
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -106,6 +108,41 @@ public class TestController {
 		
 		
 		return "/qa";
+	}
+	
+	//1
+	
+	@GetMapping("/products")
+	public String product(Model model, Product products) {
+		return "product-list";
+	}
+	
+	@GetMapping("/Jproduct")
+	public String JproductPage(Model model) {
+
+	    List<List<Jproduct>> plist = new ArrayList<>();
+
+	    for (int i = 0; i < 2; i++) {
+	        List<Jproduct> plistItem = new ArrayList<>();
+	        for (int j = 0; j < 5; j++) {
+	            Jproduct jproduct = new Jproduct();
+	            if (j % 2 == 0) {
+	                jproduct.setImg("img/hi.png");
+	            } else {
+	                jproduct.setImg("img/h2.png");
+	            }
+	           // jproduct.setImg("img/hi.png");
+	            jproduct.setName("이름" + j);  // 각 제품마다 다른 이름
+	            jproduct.setPrice((j + 1) * 10000);
+	            jproduct.setUrl("/products");
+	            plistItem.add(jproduct);
+	        }
+	       
+	        plist.add(plistItem);
+	    }
+	    model.addAttribute("plist", plist);
+
+	    return "jproduct";
 	}
 	
 }
