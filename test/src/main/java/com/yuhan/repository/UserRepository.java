b84d8c765b6212326f819900113238f0ff7540f9
leeinbeom <dqdq7979@naver.com>
2023-09-07 09:09:15 +0900

    123

--- test/src/main/java/com/yuhan/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yuhan.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.yuhan.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
