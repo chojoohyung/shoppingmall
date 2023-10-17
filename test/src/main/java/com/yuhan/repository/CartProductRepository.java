ccd73edee7de7afce7dc64878e54acef376295da
chojoohyung <terry0791@naver.com>
2023-10-17 12:05:39 +0900

    db update

--- test/src/main/java/com/yuhan/repository/CartProductRepository.java
+++ test/src/main/java/com/yuhan/repository/CartProductRepository.java
@@ -2,7 +2,6 @@ package com.yuhan.repository;
 
 import java.util.List;
 
-import org.hibernate.annotations.NamedNativeQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,8 +15,7 @@ public interface CartProductRepository extends JpaRepository<CartProduct, Long> 
 	CartProduct findByCartIdAndProductId(Long id, Long productId);
 	
 	
-	@Query("SELECT new com.yuhan.dto.CartDetailDto(cp.id, p.name, p.price, p.size, p.color, cp.count, pi.imgUrl) FROM CartProduct cp, ProductImg pi JOIN cp.product p WHERE cp.cart.id = :cartId AND pi.product.id = cp.product.id GROUP BY cp.id ORDER BY cp.id desc")
+	@Query("SELECT new com.yuhan.dto.CartDetailDto(cp.id, p.name, p.price, p.size, p.color, cp.count, pi.imgUrl) FROM CartProduct cp, ProductImg pi JOIN cp.product p WHERE cp.cart.id = :cartId AND pi.product.id = cp.product.id GROUP BY cp.id, p.name, p.price, p.size, p.color, cp.count, pi.imgUrl ORDER BY cp.id desc")
 	List<CartDetailDto> findCartDetailDtoList(@Param("cartId") Long cartId);
-	
 	
 }
